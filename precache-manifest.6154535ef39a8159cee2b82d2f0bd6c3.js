7469ef7e91ebec7bac71a14e97180641b52dc597
Lucas Andrade de Oliveira <lucasandradedeveloper@gmail.com>
2019-12-08 15:44:16 +0000

    Updates

---
+++ precache-manifest.6154535ef39a8159cee2b82d2f0bd6c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002b223fb6fb0c667ead55ce2c8cd68f",
+    "url": "/sudoku-app/index.html"
+  },
+  {
+    "revision": "5a9157b83e0ae1af0785",
+    "url": "/sudoku-app/static/css/main.4aec6476.chunk.css"
+  },
+  {
+    "revision": "72faa31ddb26659195eb",
+    "url": "/sudoku-app/static/js/2.9eb30810.chunk.js"
+  },
+  {
+    "revision": "5a9157b83e0ae1af0785",
+    "url": "/sudoku-app/static/js/main.c4a80034.chunk.js"
+  },
+  {
+    "revision": "650265c7c86e7e930dc7",
+    "url": "/sudoku-app/static/js/runtime-main.5ae81229.js"
+  }
+]);
